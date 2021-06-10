7373a0e994ee2183b8b584d6270ea8ac89b6e146
Chris Worcester <chrisworc@gmail.com>
2021-06-09 22:00:43 -0400

    Add platform actor that moves only on server

---
+++ Source/PuzzlePlatforms/Actors/MovingPlatform.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority())
+		AddActorWorldOffset(FVector(Speed * DeltaTime, 0, 0));
+}
