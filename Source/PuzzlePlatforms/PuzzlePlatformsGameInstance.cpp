d7ed61f16e921e235655627b090e5101221f758a
Chris Worcester <chrisworc@gmail.com>
2021-12-18 22:50:55 -0500

    Implement console commands to host and join servers

---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance()
+{
+	UE_LOG(LogTemp, Warning, TEXT("UPuzzlePlatformsGameInstance Constructor called."));
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	Super::Init();
+
+	UE_LOG(LogTemp, Warning, TEXT("UPuzzlePlatformsGameInstance Init called"));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Hosting..."));
+
+	UWorld* World = GetWorld();
+	if (World)
+		World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* Controller = GetFirstLocalPlayerController();
+	if (Controller)
+	{
+		Controller->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+	}
+}
